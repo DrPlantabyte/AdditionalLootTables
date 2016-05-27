9bd973f76c94fead6cda9904bfb59a52345ec966
Chris Hall <explosivegnome@yahoo.com>
2016-05-27 09:12:45 -0400

    update to 1.9.4

--- src/main/java/cyano/alt/AdditionalLootTables.java
+++ src/main/java/cyano/alt/AdditionalLootTables.java
@@ -62,7 +62,7 @@ public class AdditionalLootTables
 {
 	public static final String NAME = "Additional Loot Tables";
 	public static final String MODID = "alt";
-	public static final String VERSION = "1.03";
+	public static final String VERSION = "1.04";
 
 	public static boolean enabled = true;
 	public static boolean strict_mode = false;
